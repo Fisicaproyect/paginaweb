2f84b2f41d82a08395712fd65ef1dce96bd965c4
Fisicaproyect <117497257+Fisicaproyect@users.noreply.github.com>
2022-11-04 21:34:59 -0600

    primer pagina

---
+++ tiempo.php
@@ -0,0 +1,35 @@
+<html>
+<head> <title> Caida libre  </title>
+</head>
+<!-- FORMULAS -->
+<body background="https://static.vecteezy.com/system/resources/previews/006/329/755/non_2x/hand-drawn-math-formula-and-chemistry-formula-mathematics-background-physics-formula-physics-chemistry-formula-education-and-learning-background-vector.jpg"> <div align="center">
+<br>
+<br>
+<br>
+<br><font face="Comic Sans MS" size="5" color="white"> 
+<form method="POST" action="">
+<p><h1><font size="6" color="white">   Ingrese VELOCIDAD FINAL:</font></h1></p>
+<input type="text" name="j">
+<br> <br>
+<p><h1> <font size="6"color="white">  Ingrese VELOCIDAD INICIAL: </font></h1></p>
+<input type="text" name="k">
+<br> <br>
+<p><h1><font size="6" color="white">  Ingrese ACELERACION:</font></h1></p>
+<input type="text" name="l">
+<br>
+<br>
+<br>
+<input type="submit" value="resultado" style='width:130px; height:45px'>
+
+</form>
+     </body>
+     <?php
+$j = $_POST['j'];
+$k = $_POST['k']; 
+$l = $_POST['l']; 
+$sx = (($j-$k)/$l);
+echo  "<font size=\"6\" color=\"white\">'EL TIEMPO ES: '$sx '</font>";
+?>
+<!-- FIN FORMULA 1 -->
+<div>
+</html
